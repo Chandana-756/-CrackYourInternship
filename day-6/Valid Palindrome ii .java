0d1625de3d76db32c3006d27bec4374a3054c07c
Chandana-756 <94692989+Chandana-756@users.noreply.github.com>
2024-07-19 15:47:42 +0530

    Create Valid Palindrome ii .java

---
+++ day-6/Valid Palindrome ii .java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+            }
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
+
+Time Complexity: O(N)
+Space Complexity: O(1)
