a49410fa6a68cc5b8721e4bd857c0e34a1b3afbb
Chandana-756 <94692989+Chandana-756@users.noreply.github.com>
2024-07-23 20:16:59 +0530

    Create Minimum Moves to Equal Array Elements.java

---
+++ day-10/Minimum Moves to Equal Array Elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = nums[0];
+        int result = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] < min) min = nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            result = result + nums[i] - min;
+        }
+        return result;
+    }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(1)
