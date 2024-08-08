09daa3043ff4484b6fa18a2a1caf75ca7d7128f7
Chandana Reddy <94692989+Chandana-756@users.noreply.github.com>
2024-08-08 20:29:39 +0530

    Create Implement Stack using Queue.java

---
+++ day-26/Implement Stack using Queue.java
@@ -0,0 +1,27 @@
+class MyStack {
+
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        q.offer(x);
+        for (int i = 0; i < q.size() - 1; ++i) q.offer(q.poll());
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+    private Queue<Integer> q = new ArrayDeque<>();
+}
+
+Time Complexity: O(1)
+Space Complexity: O(n)
