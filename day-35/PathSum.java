b112b166a54df0e1c76a5bb9ba0ce41d3f9c1d62
Chandana Reddy <94692989+Chandana-756@users.noreply.github.com>
2024-08-17 19:59:52 +0530

    Create PathSum.java

---
+++ day-35/PathSum.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null)
+      return false;
+
+    // Create 2 stacks for the path and the sums
+    Stack<TreeNode> path = new Stack<>();
+    Stack<Integer> sumPath = new Stack<>();
+
+    path.push(root);
+    sumPath.push(root.val);
+
+    while (!path.isEmpty()) {
+      TreeNode temp = path.pop();
+      int tempVal = sumPath.pop();
+
+      // If a child node and we find the sum total, return true
+      if (temp.left == null && temp.right == null && tempVal == targetSum)
+        return true;
+
+      if (temp.right != null) {
+        path.push(temp.right);
+        sumPath.push(temp.right.val + tempVal);
+      }
+
+      if (temp.left != null) {
+        path.push(temp.left);
+        sumPath.push(temp.left.val + tempVal);
+      }
+
+    }
+
+    return false;
+    }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(n)
