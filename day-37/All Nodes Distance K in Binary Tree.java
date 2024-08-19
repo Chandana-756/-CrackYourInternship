a21248f71956e5aab3848662e5a75ea841b3b9d6
Chandana Reddy <94692989+Chandana-756@users.noreply.github.com>
2024-08-19 22:11:20 +0530

    Create All Nodes Distance K in Binary Tree.java

---
+++ day-37/All Nodes Distance K in Binary Tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    List<Integer> ans = new ArrayList<>();
+    Map<TreeNode, Integer> nodeToDist = new HashMap<>();
+
+    getDists(root, target, nodeToDist);
+    dfs(root, k, 0, nodeToDist, ans);
+
+    return ans;
+  }
+
+  private void getDists(TreeNode root, TreeNode target, Map<TreeNode, Integer> nodeToDist) {
+    if (root == null)
+      return;
+    if (root == target) {
+      nodeToDist.put(root, 0);
+      return;
+    }
+
+    getDists(root.left, target, nodeToDist);
+    if (nodeToDist.containsKey(root.left)) {
+      nodeToDist.put(root, nodeToDist.get(root.left) + 1);
+      return;
+    }
+
+    getDists(root.right, target, nodeToDist);
+    if (nodeToDist.containsKey(root.right))
+      nodeToDist.put(root, nodeToDist.get(root.right) + 1);
+  }
+
+  private void dfs(TreeNode root, int k, int dist, Map<TreeNode, Integer> nodeToDist,
+                   List<Integer> ans) {
+    if (root == null)
+      return;
+    if (nodeToDist.containsKey(root))
+      dist = nodeToDist.get(root);
+    if (dist == k)
+      ans.add(root.val);
+
+    dfs(root.left, k, dist + 1, nodeToDist, ans);
+    dfs(root.right, k, dist + 1, nodeToDist, ans);
+  }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(n)
