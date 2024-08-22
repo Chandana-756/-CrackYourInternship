c21bf30efbdc6ec7b9c7537dac999edda910e843
Chandana Reddy <94692989+Chandana-756@users.noreply.github.com>
2024-08-22 22:42:43 +0530

    Create Kth Smallest Element in a BST.java

---
+++ day-40/Kth Smallest Element in a BST.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public int kthSmallest(TreeNode root, int k) {
+    traverse(root, k);
+    return ans;
+  }
+
+  private int ans = -1;
+  private int rank = 0;
+
+  private void traverse(TreeNode root, int k) {
+    if (root == null)
+      return;
+
+    traverse(root.left, k);
+    if (++rank == k) {
+      ans = root.val;
+      return;
+    }
+    traverse(root.right, k);
+  }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(h)
