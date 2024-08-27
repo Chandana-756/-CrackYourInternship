8832a8a39045856ce48f895083a75961b555175c
Chandana Reddy <94692989+Chandana-756@users.noreply.github.com>
2024-08-27 22:57:50 +0530

    Create Number of Operations to Make Network Connected.java

---
+++ day-45/Number of Operations to Make Network Connected.java
@@ -0,0 +1,37 @@
+class Solution {
+  public int makeConnected(int n, int[][] connections) {
+    if (connections.length < n - 1)
+      return -1;
+
+    int numOfConnected = 0;
+    List<Integer>[] graph = new List[n];
+    Set<Integer> seen = new HashSet<>();
+
+    for (int i = 0; i < n; ++i)
+      graph[i] = new ArrayList<>();
+
+    for (int[] connection : connections) {
+      final int u = connection[0];
+      final int v = connection[1];
+      graph[u].add(v);
+      graph[v].add(u);
+    }
+
+    for (int i = 0; i < n; ++i)
+      if (seen.add(i)) {
+        dfs(graph, i, seen);
+        ++numOfConnected;
+      }
+
+    return numOfConnected - 1;
+  }
+
+  private void dfs(List<Integer>[] graph, int u, Set<Integer> seen) {
+    for (final int v : graph[u])
+      if (seen.add(v))
+        dfs(graph, v, seen);
+  }
+}
+
+Time Complexity: O(|V| + |E|)
+Space Complexity: O(|V| + |E|)
